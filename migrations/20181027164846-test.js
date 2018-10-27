db39b48b01a83ef473ebec8afa5fee8a81c4005f
Andrei Chayeuski <andreichayeuski@gmail.com>
2018-10-27 20:51:41 +0300

    lab 14 completed

---
+++ migrations/20181027164846-test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+	up: (queryInterface, Sequelize) =>
+    {
+		queryInterface.addColumn(
+			'films',
+			'genres',
+			Sequelize.STRING
+		);
+	},
+
+	down: (queryInterface, Sequelize) =>
+    {
+		queryInterface.removeColumn(
+			'films',
+			'genres'
+		);
+	}
+};
